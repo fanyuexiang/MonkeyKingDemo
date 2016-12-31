deda4cb252fe937fe0a9d1507e6cc6d64625d317
fanyuexiang <yuexiangfan@126.com>
2016-12-31 12:59:04 +0800

    Demo

---
+++ MonkeyKingDemo/MonkeyKingDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MonkeyKingDemo
+//
+//  Created by fancy on 2016/12/31.
+//  Copyright © 2016年 fancy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
